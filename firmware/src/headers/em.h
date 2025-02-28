3d4d2e3c37403d6e874aacc68cd4fcd30f842a90
Maksym Dybarskyi <maksym.dybarskyi@gymondo.com>
2025-02-28 17:44:51 +0100

    Add electromagnet pwm control

---
+++ firmware/src/headers/em.h
@@ -0,0 +1,12 @@
+/*
+Electro magnet control.
+Frequency of PWM can be changed.
+*/
+
+#ifndef HEADER_EM
+#define HEADER_EM
+
+void configure_magnet();
+void set_magnet_frequency(float freq);
+
+#endif // HEADER_EM

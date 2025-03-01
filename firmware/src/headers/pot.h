dac958236912fcdc6e594a4bde9aa5b9dfe168d7
Maksym <maxim.dybarskyy@gmail.com>
2025-03-01 03:54:42 +0100

    Fix potentiometer reading

--- firmware/src/headers/pot.h
+++ firmware/src/headers/pot.h
@@ -7,6 +7,6 @@ Read data is float in range [0; 1]
 #define HEADER_POT
 
 void configure_pot();
-void event_pot_value(float value);
+void on_pot_sample(float value);
 
 #endif // HEADER_POT
--- firmware/src/sources/pot.c
+++ firmware/src/sources/pot.c
@@ -1,48 +1,65 @@
 /*
-Reads data from potentiometer using ADC.
+frequency = 1 MHz
+divider = 4
+1 sec = 250 000
+frequency = 10 Hz = 25 000
 */
 
 #include <msp430g2553.h>
-#include "../headers/pot.h"
+#include "pot.h"
 
-#define RES 1000.0f
-#define PIN BIT7
-#define CHANNEL INCH_6
+
+#define PORT_SEL	P1SEL
+#define PORT_SEL2	P2SEL2
+#define PIN_POT		BIT6
+#define CHANNEL		INCH_6
+#define COUNTER		25000
+#define RESOLUTION	1000
 
 void configure_pot() {
-	P1SEL |= PIN;
-	P1SEL2 |= PIN;
-	// set default voltage reference
-	ADC10CTL0 |= SREF_0;
-	// set 16 cycles
-	ADC10CTL0 |= ADC10SHT_2;
-	// set on
-	ADC10CTL0 |= ADC10ON;
-	// set input channel
-	ADC10CTL1 |= CHANNEL;
-	// set sample source
-	ADC10CTL1 |= SHS_0;
-	// set clock divider
-	ADC10CTL1 |= ADC10DIV_0;
-	// set clock
-	ADC10CTL1 |= ADC10SSEL_3;
-	// repeat single channel conversion
-	ADC10CTL1 |= CONSEQ_2;
+	// ADC
+	// Set pin as input
+	PORT_SEL |= PIN_POT;
+	// Set pint as analog
+	PORT_SEL2 |= PIN_POT;
+	// set voltage reference, 16 cycles, on
+	ADC10CTL0 |= SREF_0 + ADC10SHT_1 + ADC10ON;
+	// set channel, sample source, clock divider, clock, single channel conversion
+	ADC10CTL1 |= CHANNEL + SHS_0 + ADC10DIV_0 + ADC10SSEL_0 + CONSEQ_0;
 	// enable analog input
-	ADC10AE0 = PIN;
+	ADC10AE0 = PIN_POT;
 	// enable interruptions
 	ADC10CTL0 |= ADC10IE;
-	// enable conversion
-	ADC10CTL0 |= ENC;
+	// Timer
+	// sum-main clock + freq divider 2 + up mode + init
+	TA0CTL = TASSEL_2 + ID_3 + MC_1 + TACLR;
+	// counter
+	TA0CCR0 = COUNTER;
+	// enable interruptions
+	TA0CCTL0 |= CCIE;
+}
+
+static void start_adc() {
 	// start conversion
-	ADC10CTL0 |= ADC10SC;
+	ADC10CTL0 |= ADC10SC + ENC;
+}
+
+#pragma vector = TIMER0_A0_VECTOR
+__interrupt void on_timer0_ccr0(void) {
+	// request adc sample
+	start_adc();
+	// reset interruption flag
+	TA0CCTL0 &= ~CCIFG;
 }
 
 #pragma vector = ADC10_VECTOR
 __interrupt void on_adc_irs() {
-	const unsigned int input = ADC10MEM;
-	const float value = input / RES;
-	event_pot_value(value);
+	double input = ADC10MEM;
+	float value = input / RESOLUTION;
+	if (value > 1) {
+		value = 1;
+	}
+	on_pot_sample(value);
+	// clear interruption flag 
+	ADC10CTL0 &= ~ADC10IFG;
 }
-
-
